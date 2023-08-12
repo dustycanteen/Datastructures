1180744205f1b84728f3e56ac95efac985878de5
Ruy@DustyCanteen <dustycanteen@gmail.com>
2023-08-12 14:24:27 +0800

    Finished Memory Arena

--- driver.cpp
+++ driver.cpp
@@ -75,7 +75,7 @@ struct StackCursor{
 
 template<typename T>
 struct Node_sll{
-    Node_sll *next;
+    Node_sll<T> *next;
     T data;
     Node_sll(T in_data):next(NULL), data(in_data){}
 };
@@ -87,7 +87,7 @@ struct ListState{
 };
 
 template<typename T>
-struct LinkedList{
+struct SLinkedList{
     Node_sll<T> *Next(Node_sll<T> *self){
         return self->next;
     }
@@ -176,7 +176,7 @@ struct SLLAllocator{
         if(free_list == NULL){
             return NULL;
         }
-        LinkedList<T> cursor;
+        SLinkedList<T> cursor;
         Node_sll<T> *front = cursor.Calve(&free_list);
         if(used_list){
             cursor.Append(front, used_list);
@@ -187,7 +187,7 @@ struct SLLAllocator{
 
     int Free(T *item){
         int result = 0;
-        LinkedList<T> cursor;
+        SLinkedList<T> cursor;
         if(mem->InRange((item))){
             Node_sll<T> *node = (Node_sll<T> *)((char *)item - sizeof(void *));
             if(node == used_list){
@@ -211,7 +211,7 @@ struct SLLAllocator{
     }
 
     void Print(void(*print_fn)(T)){
-        LinkedList<T> cursor;
+        SLinkedList<T> cursor;
         Node_sll<T> *current = free_list;
         printf("Free Nodes:\n");
         cursor.PrintContents(free_list, print_fn);
@@ -231,6 +231,12 @@ struct ArenaHeader{
 };
 
 struct Arena{
+private:
+    ArenaHeader *Previous(ArenaHeader *current){
+        return (ArenaHeader *)(((char *)current) - current->prev);
+    }
+public:
+
     RawBuffer *buffer;
     ArenaHeader *top;
 
@@ -242,15 +248,18 @@ struct Arena{
 
     ArenaHeader *Push(int arena_size_bytes){
         int allocation_size = ((arena_size_bytes + 7) / 8) * 8; //round allocation to nearest 64 bytes
-
+        printf("Arena allocation size: %d\n", allocation_size);
         ArenaHeader *item = top;
         item->size=allocation_size;
         char *arena_top_bytes = (char*)top;
+        printf("Arena Top before push: %ld\n", arena_top_bytes - (char *)buffer->base);
         arena_top_bytes += allocation_size + sizeof(ArenaHeader);
+        printf("Arena Top after push: %ld\n", arena_top_bytes - (char *)buffer->base);
+
 
         if(buffer->InRange((void *)arena_top_bytes)){
             top = (ArenaHeader *)arena_top_bytes;
-            top->prev = (int)(top - item);
+            top->prev = (int)((char *)top - (char *)item);
             
             return item;
         }
@@ -262,65 +271,36 @@ struct Arena{
     void Pop(){
         if(top->prev == 0){
             //already at base
+            top->size = 0;
         }
         else{
             char *arena_top_bytes = (char *)top;
             arena_top_bytes -= top->prev;
             top = (ArenaHeader *)arena_top_bytes;
+            top->size = 0;
 
             assert(buffer->InRange((void *)top));
         }
     }
 
     void Print(){
-        ArenaHeader *current = top;
-        printf("Printing Arena Allocations\n");
-        while(current->prev != 0){
-            printf("Arena [Size|Prev]: [%d|%d]", current->size, current->prev);
-            current = (ArenaHeader *)(((char *)current) - current->prev);
+        ArenaHeader *current = Previous(top);
+        printf("Printing Arena Allocations\n\t[T");
+        do{
+            printf(" %d ", current->size);
+            current = Previous(current);
             if(!buffer->InRange((void *)current)){
                 printf("Error: Traversal down stack invalid\n");
                 break;
             }
-        }
+        }while(current->prev != 0);
+        printf("B]\n");
     }
 };
 
 int main(){
     RawBuffer stack_memory("Stack memory", 128);
-    StackState<int> state(stack_memory.base,stack_memory.size);
-    StackCursor<int> stack_cursor;
-
-    int count = 0;
-    while(!state.Full()){
-        state.next = stack_cursor.Push(state.next,count++);
-    }
-    while(state.Valid()){
-        printf("Next stack val: %d\n", stack_cursor.Pop(&state.next));
-    }
-    printf("Count traversed: %d\n", count);
-    printf("next stack val: %d\n", *(int *)state.base);
-
-    RawBuffer list_memory("List Memory", sizeof(Node_sll<int>) * 16);
     
-
-    RawBuffer arena_memory("Arena memory", 512);
-    Arena arena(&arena_memory);
-
-    arena.Print();
-    ArenaHeader *allocation_1 = arena.Push(8);
-    ArenaHeader *allocation_2 = arena.Push(12);
-    ArenaHeader *allocation_3 = arena.Push(16);
-    ArenaHeader *allocation_4 = arena.Push(4);
-    printf("Allocation 1 location: %p\n", allocation_1);
-    printf("Allocation 2 location: %p\n", allocation_2);
-    printf("Allocation 3 location: %p\n", allocation_3);
-    printf("Allocation 4 location: %p\n", allocation_4);
-    printf("2-1: %ld\n", allocation_2 - allocation_1);
-    printf("3-2: %ld\n", allocation_3 - allocation_2);
-    printf("4-3: %ld\n", allocation_4 - allocation_3);
-    printf("top-4: %ld\n", arena.top - allocation_4);
-    arena.Print();
 
     return 0;
 }
